1fd2ec2698731fd7e38c86c792f1710e94fbe8f5
Akash Kumar <98875003+akaashvaa@users.noreply.github.com>
2023-12-23 20:15:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <>
       <div className="container " style={{ marginTop: '8rem' }}>
-        <h1 className="my-4 text-center display-2">
+        <h1 className="mt-4 mb-8 text-center display-2">
           Qr Code Generator & Scanner
         </h1>
 
-        <div className="row pt-4 mt-8">
+        <div className="row pt-4 mt-6">
           <CreateQR />
           <ReadQR />
         </div>
